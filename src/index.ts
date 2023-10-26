bd01876cb79bbdb0500f2da7cb4ddff628dfc0c2
AMALU SAJEEV <amalu.sajeev.me@gmail.com>
2023-10-26 22:16:22 +0530

    build(code): added path aliases using link-module-alias

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import '../bin/config';
-import { LoggerAdapter } from '../adapters/logger.adapter';
-// import '@bin/config';
+import '@bin/config';
+import { LoggerAdapter } from '@adapters/logger.adapter';
 import { container } from 'tsyringe';
 
 (function main() {
     const scream = container.resolve(LoggerAdapter);
-    // write your code here..
     scream.info('hello world');
 })();
